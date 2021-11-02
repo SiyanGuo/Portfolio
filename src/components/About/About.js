e345fc89e4e8297b1474e37799c0d5182049349d
Siyan Guo <siyang@Hey-There.local>
2021-11-01 21:41:35 -0400

    add About Me component

---
+++ src/components/About/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import myPic from '../../assets/siyan.jpg';
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+            <div className="flex-row">
+                <img src={myPic} className="space-between mx-5" style={{ width: "20%" }} alt="Siyan's portrait" />
+                <div className="space-between mx-5 container">
+                    <p style={{ width: "100%" }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+                        condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+                        mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
+                        Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
+                        justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
+                        dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
+                        Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+                    </p>
+                    <p style={{ width: "100%" }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec bibendum turpis sed ex
+                        condimentum molestie.Mauris condimentum lectus ut ornare dignissim.Mauris faucibus urna
+                        mi, ac feugiat metus aliquam maximus.Proin aliquam justo nec diam vulputate vestibulum.
+                        Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend.Morbi tristique
+                        justo vel turpis sollicitudin, et tristique velit convallis.In hac habitasse platea
+                        dictumst.Phasellus mattis nunc sed orci consequat laoreet.Praesent id nisl nibh.
+                        Curabitur imperdiet ultricies mollis.In hac habitasse platea dictumst.
+                    </p>
+                </div>
+            </div>
+
+
+
+        </section>
+    );
+}
+
+export default About;
