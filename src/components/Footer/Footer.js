985e15d7cc32f3923df0f2269adaed6950ae24e8
Siyan Guo <siyang@Hey-There.local>
2021-11-03 23:46:19 -0400

    add links in the footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
 const Footer = () => {
-    var style = {
-        backgroundColor: "#F8F8F8",
-        borderTop: "1px solid #E7E7E7",
-        textAlign: "center",
-        padding: "20px",
-        position: "fixed",
-        left: "0",
-        bottom: "0",
-        height: "60px",
-        width: "100%",
-    }
+    // var style = {
+    //     backgroundColor: "#F8F8F8",
+    //     borderTop: "1px solid #E7E7E7",
+    //     textAlign: "center",
+    //     padding: "20px",
+    //     position: "fixed",
+    //     left: "0",
+    //     bottom: "0",
+    //     height: "60px",
+    //     width: "100%",
+    // }
 
-    var phantom = {
-        display: 'block',
-        padding: '20px',
-        height: '60px',
-        width: '100%',
-    }
+    // var phantom = {
+    //     display: 'block',
+    //     padding: '20px',
+    //     height: '60px',
+    //     width: '100%',
+    // }
 
     return (
 
-        <footer style={phantom}>
-            <div stype={style}>
-                <p>Code with 🧡  by Siyan Serena Guo</p>
+        <footer >
+            <div >
+                <a href="https://github.com/SiyanGuo" rel="noreferrer" target="_blank"><i class="fab fa-github-square icon"></i> </a>
+                <a href="https://www.linkedin.com/in/serenasiyanguo/" rel="noreferrer" target="_blank"><i class="fab fa-linkedin icon"></i></a>
+                <a href="https://twitter.com/guo_siyan" rel="noreferrer" target="_blank"><i class="fab fa-twitter-square icon"></i> </a>
             </div>
         </footer>
     )
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,23 +6,18 @@ const Portfolio = () => {
     // const test = portfolioList[0]
     // console.log("test", test);
     return (
-        <div className=" flex-row">
-            {/* <h2 >Portfolio</h2>
-            <div>
-                <img src={require('../../assets/blog.jpg').default} alt="portfolio1"></img>
-                <h4>{test.name}</h4>
-                <a href={test.github} rel="noreferrer" target="_blank">Github</a>
-                <a href={test.deploy} rel="noreferrer" target="_blank">Deployed</a>
-            </div> */}
-
-            {portfolioList.map(portfolio => (
-            <Project
-              id={portfolio.id}
-              name={portfolio.name}
-              github={portfolio.github}
-              deploy={portfolio.deploy}
-            />
-          ))}
+        <div className="container">
+            <h2 >Portfolio</h2>
+            <div className="flex-row">
+                {portfolioList.map(portfolio => (
+                    <Project
+                        id={portfolio.id}
+                        name={portfolio.name}
+                        github={portfolio.github}
+                        deploy={portfolio.deploy}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
